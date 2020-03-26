fac38e7e7d5b89e2944078eae3a841f2bc700349
Alzbeta Jurecekova <ajurecekova@gmai.com>
2020-03-26 09:13:48 +0100

    for cykly

---
+++ zadanie 1.3.17.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdlib.h>
+
+char nahodna_permutacia_cyklickeho_typu(int *perm, int *cyc, int ncyc){
+int i,j,k,n=0,m;
+for (i=0;i<ncyc;i++){
+	n+=cyc[i];
+}
+for (j=0;j<ncyc;j++){
+	for(k=0;k<cyc[j];k++){
+		
+	}
+}	
+}
