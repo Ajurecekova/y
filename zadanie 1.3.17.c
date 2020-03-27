99dc2c7257d09846b7ff289e5e0886cd9692328e
Alzbeta Jurecekova <ajurecekova@gmai.com>
2020-03-27 12:32:25 +0100

    funguje myslim spravne

--- zadanie 1.3.17.c
+++ zadanie 1.3.17.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 
-char nahodna_permutacia_cyklickeho_typu(int *perm, int *cyc, int ncyc){
+char nahodna_permutacia_cyklickeho_typu(int *p, int *cyc, int ncyc){
 int i,j,k,n=0,m,a,b,c,match;
-
+printf("%d\n",ncyc);
 for (i=0;i<ncyc;i++){
 	n+=cyc[i];
 }
-int r[n],p[n+1];
+int r[n+1];
 //pole nahodnych cisel po n okrem 1
 printf("%d \n",n);
-for (j=0;j<n-1;j++){
+for (j=0;j<n;j++){
 	do{
 	match = 0;
-	r[j]=(rand()%(n-1))+2;
+	r[j]=(rand()%(n))+1;
 	for (i=0;i<j;i++){
 		if (r[i] == r[j]){
 			match=1;
@@ -23,20 +23,22 @@ for (j=0;j<n-1;j++){
 	}while(match!=0);
 }
 //vypis pola
-for(i=0;i<n-1;i++)
-	printf("%d",r[i]);
+for(i=0;i<n;i++)
+	printf("%d ",r[i]);
 printf("\n");
 
 //vytvorenie cyklickej permutacie
-m=rand()%n;
+m=0;
+a=0;
+
 for(i=0;i<ncyc;i++){
-	p[m]=r[cyc[i]-2];
-	for(j=1;j<=cyc[i]-2;j++){
-		p[r[cyc[i]-j-1]-1]=r[cyc[i]-j-2];
-		a=r[cyc[i]-j-2];
+	a+=cyc[i];
+	p[r[m]-1]=r[a-1];
+	for(j=1;j<=cyc[i]-1;j++){
+		p[r[a-j]-1]=r[a-j-1];
+		
 	}
-	p[a-1]=m+1;
-	m=cyc[i+1];
+	m+=cyc[i];
 }
 
 //vypis permutacie
@@ -44,14 +46,18 @@ for (j=0;j<n;j++){
 	printf("%d ",p[j]);
 }
 
-	
+printf("\n%d ",*p);
+return(*p);	
 }
 
 int main(){
-	int *perm;
-	int *cyc;
-	int ncyc=1,m=7;
+	int perm[100];
+	int cyc[10];
+	int ncyc = 3;
+	
 	srand(time(NULL));
-	cyc=&m;
+	cyc[0]=4;
+	cyc[1]=3;
+	cyc[2]=5;
 	nahodna_permutacia_cyklickeho_typu(perm,cyc,ncyc);
 }
